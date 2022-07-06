4b8ca8f7702e51ce564438410438d25846fa99f7
AFSantos00 <95322131+AFSantos00@users.noreply.github.com>
2022-07-06 08:58:51 -0300

    adicionando metodo post

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,12 +4,32 @@ async function get(req, res) {
     const { id } = req.params
 
     const obj = id ? { _id: id } : null
-    
+
     const products = await ProductsModel.find(obj)
     
     res.send(products)
 }
 
+async function post(req, res) {
+    const {
+        name,
+        brand,
+        price,
+    } = req.body
+
+    const product = new ProductsModel({
+        name,
+        brand,
+        price,
+    })
+
+    product.save()
+    res.send({
+        message: 'success'
+    })
+}
+
 module.exports = {
     get,
+    post,
 }
